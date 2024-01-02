fe0ce4591fa92f0d26a0b20be405bab8817d5ef5
joks98 <joksanhaniel@gmail.com>
2024-01-02 14:12:08 -0600

    Cambios menores

--- src/env.js
+++ src/env.js
@@ -1,9 +1,9 @@
 (function (window) {
     window.__env = window.__env || {};
-  
+
     // API url
-    window.__env.apiUrl = 'http://181.189.242.18:3000/';
-  
+    window.__env.apiUrl = 'http://JOKSAN-IT:3000/';
+
     // Whether or not to enable debug mode
     // Setting this to false will disable console output
     window.__env.enableDebug = true;
