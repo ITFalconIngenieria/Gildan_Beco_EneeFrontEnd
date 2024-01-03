c6ccd9de86b295078dcc79245384528c604fd04f
joks98 <joksanhaniel@gmail.com>
2024-01-03 13:08:04 -0600

    Version PRD

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment =  {
   production: true,
-  api:'http://RIO6SPSSM1:3000/'
+  api:'http://localhost:3000/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  api: "http://RIO6SPSSM1:3000/"
+  api: "http://localhost:3000/"
 };
 
 /*
